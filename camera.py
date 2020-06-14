ecb6bfd9fd0aa02f67f7bdd3a133736df6db845d
ameyaphatak88 <ameyaphatak88@gmail.com>
2020-06-14 18:43:16 +0530

    camera.y count number of frames in a video

---
+++ camera.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+import sys
+
+def video_capt(filepath):
+	assert os.path.exists(filepath), "video file path is not valid"
+	video1 = cv2.VideoCapture(filepath)
+
+	length = int(video1.get(cv2.CAP_PROP_FRAME_COUNT))
+	print(length)
+
+if __name__ == '__main__':
+	assert len(sys.argv) == 2, "Number of arguments not equal to 2"
+	video_capt(str(sys.argv[1]))
+
---
+++ hi.py
@@ -0,0 +1,5 @@
+import camera
+
+camera.video_capt()
+
+
