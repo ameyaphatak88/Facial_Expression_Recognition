a3719f4fc5924c32285e7101de9fe3c3436b3c90
ameyaphatak88 <ameyaphatak88@gmail.com>
2020-06-15 21:08:15 +0530

    a

--- camera.py
+++ camera.py
@@ -1,6 +1,13 @@
 import cv2
 import os
 import sys
+from model import FacialExpressionModel
+import numpy as np
+
+facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+model = FacialExpressionModel("model.json", "model_weights.h5")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 
 def video_capt(filepath):
 	assert os.path.exists(filepath), "video file path is not valid" + filepath
@@ -9,7 +16,26 @@ def video_capt(filepath):
 	length = int(video1.get(cv2.CAP_PROP_FRAME_COUNT))
 	return(length)
 
-if __name__ == '__main__':
-	assert len(sys.argv) == 2, "Number of arguments not equal to 2"
-	video_capt(str(sys.argv[1]))
+#if __name__ == '__main__':
+#	assert len(sys.argv) == 2, "Number of arguments not equal to 2"
+#	video_capt(str(sys.argv[1]))
+
+def picture_capt(filepath):
+	assert os.path.exists(filepath), "photo file path is not valid" + filepath
+	image1 = cv2.imread(filepath, 0)
+	gray_fr = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+	return gray_fr.size
+
+
+#def emotion(filepath):
+#	assert os.path.exists(filepath), "file path is not valid" + filepath
+#	image1 = cv2.imread(filepath, 0)
+#	gray_fr = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+#	#faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+#	#emotion_predicted = []
+
+#	return("h")
+
+
+print(picture_capt('ameya.jpeg'))
 
--- video_server.py
+++ video_server.py
@@ -21,5 +21,15 @@ def print_frames_for(filename):
 	else:
 		return("File path is not valide, plz enter the correct path")
 
+
+@app.route('/images/')
+def print_image_size():
+	return(str(camera.picture_capt('ameya.jpeg')))
+
+
+@app.route('/emotion')
+def pred_emotion():
+	return(str(camera.emotion('ameya.jpeg')))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
